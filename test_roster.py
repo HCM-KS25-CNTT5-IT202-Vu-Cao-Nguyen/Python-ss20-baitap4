58a785cfc3b2ebecfcbfe3aa94833d59b9cbe543
Vu Cao Nguyen <nguyencaovu2007@gmail.com>
2026-06-15 14:33:21 +0700

    sửa bài tập sai

--- test_roster.py
+++ test_roster.py
@@ -1,33 +1,45 @@
-import unittest
+mport unittest
 
 from main import calculate_actual_pay
 
 
 class TestCalculateActualPay(unittest.TestCase):
 
-    def test_active_player(self):
+    def test_active_player_should_receive_full_salary(self):
+        """
+        Active -> nhận 100% lương
+        """
 
         player = {
+            "player_id": "P001",
+            "name": "Levi",
             "salary": 5000,
             "status": "Active"
         }
 
-        self.assertEqual(
-            calculate_actual_pay(player),
-            5000
-        )
+        expected_result = 5000
 
-    def test_benched_player(self):
+        actual_result = calculate_actual_pay(player)
+
+        self.assertEqual(expected_result, actual_result)
+
+    def test_benched_player_should_receive_half_salary(self):
+        """
+        Benched -> nhận 50% lương
+        """
 
         player = {
-            "salary": 6000,
+            "player_id": "P002",
+            "name": "Kati",
+            "salary": 4000,
             "status": "Benched"
         }
 
-        self.assertEqual(
-            calculate_actual_pay(player),
-            3000
-        )
+        expected_result = 2000
+
+        actual_result = calculate_actual_pay(player)
+
+        self.assertEqual(expected_result, actual_result)
 
 
 if __name__ == "__main__":
